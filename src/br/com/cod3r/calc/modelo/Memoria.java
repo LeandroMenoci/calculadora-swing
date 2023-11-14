68f9666b512ef1c6b141646966694e71621cc0ae
leandromenoci <menoci.leandro@gmail.com>
2023-11-13 22:07:18 -0300

    Finalizando calculadora

--- src/br/com/cod3r/calc/modelo/Memoria.java
+++ src/br/com/cod3r/calc/modelo/Memoria.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Memoria {
 
     private enum TipoComando {
-        ZEREAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+        ZEREAR, SINAL, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
     }
 
     private static final Memoria instancia = new Memoria();
@@ -43,6 +43,10 @@ public class Memoria {
             textoBuffer = "";
             substituir = false;
             ultimaOperacao = null;
+        } else if (tipoComando == TipoComando.SINAL && textoAtual.contains("-")) {
+            textoAtual = textoAtual.substring(1);
+        }else if (tipoComando == TipoComando.SINAL && !textoAtual.contains("-")) {
+            textoAtual = "-" + textoAtual;
         } else if (tipoComando == TipoComando.NUMERO || tipoComando == TipoComando.VIRGULA) {
             textoAtual = substituir ? texto : textoAtual + texto;
             substituir = false;
@@ -57,7 +61,7 @@ public class Memoria {
     }
 
     private String obterResultadoOperacao() {
-        if (ultimaOperacao == null) {
+        if (ultimaOperacao == null || ultimaOperacao == TipoComando.IGUAL) {
             return textoAtual;
         }
 
@@ -104,6 +108,8 @@ public class Memoria {
                 return TipoComando.SUB;
             } else if ("=".equals(texto)) {
                 return TipoComando.IGUAL;
+            }else if ("±".equals(texto)) {
+                return TipoComando.SINAL;
             } else if (",".equals(texto) && !textoAtual.contains(",")) {
                 return TipoComando.VIRGULA;
             } else {
